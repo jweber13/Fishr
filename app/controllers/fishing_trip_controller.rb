1076d34828c03c72a3f83cbf72f59648d21e00a4
Donald Chow <donald7290@gmail.com>
2023-06-13 11:43:49 +0900

    set up basic routes and authorization of Trip

--- app/controllers/fishing_trip_controller.rb
+++ app/controllers/fishing_trip_controller.rb
@@ -1,9 +1,15 @@
 class FishingTripController < ApplicationController
 
+  # IMPORTANT subject to change based on how the view is design without a fishing trip and how it will be shown.
+
   def new
+    @fishing_trip = FishingTrip.new
+    authorize @fishing_trip
   end
 
   def show
+    @fishing_trip = FishingTrip.find(params[:id])
+    authorize @fishing_trip
   end
 
 end
---
+++ app/policies/fishing_trip_policy.rb
@@ -0,0 +1,16 @@
+class FishingTripPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def new?
+    true
+  end
+
+  def show?
+    user == record.user
+  end
+end
