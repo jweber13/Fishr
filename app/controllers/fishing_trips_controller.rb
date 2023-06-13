7e8304c72a17940cf1ee9cb6cdca6ca53acf1134
Donald Chow <donald7290@gmail.com>
2023-06-13 12:10:23 +0900

    added strong params for fishing trip

--- app/controllers/fishing_trips_controller.rb
+++ app/controllers/fishing_trips_controller.rb
@@ -1,11 +1,19 @@
 class FishingTripsController < ApplicationController
-  def new
-    @fishing_trip = FishingTrip.new
+  def create
+    @fishing_trip = FishingTrip.new(fishing_trip_params)
+    @fishing_trip.user = current_user
+    @fishing_trip.save
     authorize @fishing_trip
   end
 
   def show
     @fishing_trip = FishingTrip.find(params[:id])
     authorize @fishing_trip
+  end
+
+  private
+
+  def fishing_trip_params
+    params.require(:job).permit(:user, :name)
   end
 end
